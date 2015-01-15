660361fe220aa2a924eeab6a9aa405f41aec0091
George Yong <geoyws@gmail.com>
2015-01-15 16:25:43 +0800

    alright

---
+++ js/app.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+/* App Module */
+
+var mallApp = angular.module('mallApp', [
+    'mallAppAnimations',
+    'mallAppControllers',
+    'mallAppFilters',
+    'mallAppServices'
+]);
+
+mallApp.config(
+    [
+        '$routerProvider',
+        function ($routeProvider) {
+            $routeProvider.
+                when('/home', {
+                    templateUrl: 'partials/home.html',
+                    controller: 'homeCtrl'
+                })
+                when('/customer-service', {
+                    templateUrl: 'partials/customer-service.html',
+                    controller: 'customerServiceCtrl'
+                }).
+                when('/directory', {
+                    templateUrl: 'partials/directory.html',
+                    controller: 'directoryCtrl'
+                }).
+                otherwise({
+                    redirectTo: '/home'
+                });
+        }
+    ]
+);
