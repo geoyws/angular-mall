7de419a0c1eb28bc344fb2f8d3169a9ecbb8ec10
George Yong <geoyws@gmail.com>
2015-01-15 17:41:08 +0800

    alrighty

--- js/app.js
+++ js/app.js
@@ -17,14 +17,38 @@ mallApp.config(
                 when('/home', {
                     templateUrl: 'partials/home.html',
                     controller: 'homeCtrl'
-                })
-                when('/customer-service', {
-                    templateUrl: 'partials/customer-service.html',
-                    controller: 'customerServiceCtrl'
+                }).
+                when('/start', {
+                    templateUrl: 'partials/start.html',
+                    controller: 'startCtrl'
+                }).
+                when('/login', {
+                    templateUrl: 'partials/login.html',
+                    controller: 'loginCtrl'
+                }).
+                when('/select-default', {
+                    templateUrl: 'partials/select-default.html',
+                    controller: 'selectDefaultCtrl'
                 }).
                 when('/directory', {
                     templateUrl: 'partials/directory.html',
                     controller: 'directoryCtrl'
+                }).
+                when('/whats-happening', {
+                    templateUrl: 'partials/whats-happening.html',
+                    controller: 'whatsHappeningCtrl'
+                }).
+                when('/transactions', {
+                    templateUrl: 'partials/center-info.html',
+                    controller: 'centerInfoCtrl'
+                }).
+                when('/center-info', {
+                    templateUrl: 'partials/center-info.html',
+                    controller: 'centerInfoCtrl'
+                }).
+                when('/qr', {
+                    templateUrl: 'partials/qr.html',
+                    controller: 'qrCtrl'
                 }).
                 otherwise({
                     redirectTo: '/home'
--- js/controllers.js
+++ js/controllers.js
@@ -1,1 +1,22 @@
-﻿
+﻿'use strict';
+
+/* Controllers */
+
+var mallAppControllers = angular.module('mallAppControllers', []);
+
+mallAppControllers.controller('homeCtrl', ['$scope', 'Phone',
+  function ($scope, Phone) {
+      $scope.phones = Phone.query();
+      $scope.orderProp = 'age';
+}]);
+
+mallAppControllers.controller('startCtrl', ['$scope', '$routeParams', 'Phone',
+  function ($scope, $routeParams, Phone) {
+      $scope.phone = Phone.get({ phoneId: $routeParams.phoneId }, function (phone) {
+          $scope.mainImageUrl = phone.images[0];
+      });
+
+      $scope.setImage = function (imageUrl) {
+          $scope.mainImageUrl = imageUrl;
+      }
+}]);
