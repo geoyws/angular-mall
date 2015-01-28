37334fec69b25d0b908dca4114fc1ef17a8f76e9
geoyws <geoyws@gmail.com>
2015-01-28 17:48:53 +0800

    hurraaay

--- js/controllers.js
+++ js/controllers.js
@@ -6,26 +6,39 @@ var mallAppControllers = angular.module('mallAppControllers', ['ngMaterial'])
 .controller('splashscreenCtrl', ['$scope', '$window', '$location', '$timeout',
   function ($scope, $window, $location, $timeout) {
     $scope.splashRedirect = $timeout(function () {
-      //alert(JSON.stringify(localStorage.getItem('userId')));
-      //alert(JSON.stringify(window.localStorage));
-      // testing for localStorage functionality, if there's none, we alert a warning
       if ($window.localStorage) {
-	//alert(2);
-	if ($window.localStorage.getItem('userId')) {
-	  //alert(1);
-	  $location.path('/home');
+	var userId = $window.localStorage.getItem('userId'); // returns user's id
+	var page = $window.localStorage.getItem('homepageSetting'); // returns a page in string format
+	if (userId && page) {
+	  if (page == 'whats-happening' || 'directory') {
+	    $location.path(page);
+	  } else {
+	    $window.alert('Your home page is not set. Please set it again.');
+	    $location.path('/start');
+	  }
 	} else {
 	  $location.path('/login');
 	}
       } else {
-	alert('Your browser is outdated. This app cannot be run on your stack.');
+	$window.alert('Your browser is outdated. This app cannot be run on your stack.');
       }
     }, 500);
   }
 ])
-.controller('startCtrl', ['$scope',
-  function ($scope) {
-      $scope.whatever = 1;
+.controller('startCtrl', ['$scope', '$location', '$window',
+  function ($scope, $location, $window) {
+    $scope.startChoice = function (page) {
+      if (page == 'whats-happening') {
+	$window.localStorage.setItem('homepageSetting', 'whats-happening');
+	$location.path('/whats-happening');
+      } else
+      if (page == 'directory') {
+	$window.localStorage.setItem('homepageSetting', 'directory');
+	$location.path('/directory');
+      } else {
+	$window.alert('Invalid page chosen.');
+      }
+    }
   }
 ])
 .controller('loginCtrl', ['$scope', '$location', '$mdSidenav',
